1205ecd52abed1e33480675e54904c24c4726983
KowsarRahman <s3825159@student.rmit.edu.au>
2021-09-21 14:02:01 +0600

    Front-End testings added

--- FrontEnd/myfirstapp/src/__test__/TermsAndConditions.test.js
+++ FrontEnd/myfirstapp/src/__test__/TermsAndConditions.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from '../components/Layout/Header';
+import TermsAndConditions from '../components/Layout/TermsAndConditions';
 import {render} from '@testing-library/react';
 import renderer from 'react-test-renderer';
 
 
-it("renders Header component crashing",() => {
+
+
+it("renders Terms and Conditions component crashing",() => {
     const dev = document.createElement("dev");
-    ReactDOM.render(<Header></Header>, dev);
+    ReactDOM.render(<TermsAndConditions/>, dev);
 })
+
