bf86cfef710b332fd1bdf0d49851871779438b9f
Dunith Nadvi Karawita s3823697 <s3823697@student.rmit.edu.au>
2021-10-22 00:39:50 +0530

    Fixed Review Ordering Issue

--- BackEnd/reviewmicroservices/src/main/java/com/rmit/sept/reviewmicroservices/Web/ReviewController.java
+++ BackEnd/reviewmicroservices/src/main/java/com/rmit/sept/reviewmicroservices/Web/ReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -29,8 +30,10 @@ public class ReviewController {
     }
 
     @GetMapping("/findReviews")
-    public List<Review> findReviews(){
-        return reviewService.getReviews();
+    public List<Review> findReviews() {
+        List<Review> returnList = reviewService.getReviews();
+        Collections.reverse(returnList);
+        return returnList;
     }
 
     @GetMapping("/findReviewsById/{Id}")
@@ -40,7 +43,9 @@ public class ReviewController {
 
     @GetMapping("/findReviewsByUsername/{username}")
     public List<Review> findReviewsByUsername(@PathVariable String username) {
-        return reviewService.getReviewsByUsername(username);
+        List<Review> returnList = reviewService.getReviewsByUsername(username);
+        Collections.reverse(returnList);
+        return returnList;
     }
 
     @GetMapping("/findReviewsByISBN/{ISBN}")
